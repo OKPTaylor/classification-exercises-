a6a39c6df7cd80aba9658c9c364ae4ef299b81fb
Oliver k Taylor <o.k.p.taylor@gmail.com>
2023-04-22 21:54:06 -0500

    added stuff to prepare.py

--- prepare.py
+++ prepare.py
@@ -23,32 +23,50 @@ def split_function(df_name, target_varible_column_name):
 #call should look like: 
 #train_df_name, validate_df_name, test_df_name = split_function(df_name, 'target_varible_column_name')
 
-#This function is for running through catagorical on catagorical features graphing and running the chi2 test on them
-def cat_on_cat_graph_loop(dataframe_name, dataframe_train_name, col_cat):
+#This makes two lists containing all the categorical and continuous variables
+def cat_and_num_lists(df_train_name):
+    col_cat = [] #this is for my categorical varibles
+    col_num = [] #this is for my numeric varibles
+
+    for col in df_train_name.columns[1:]:
+        print(col)
+        if df_train_name[col].dtype == 'O':
+            col_cat.append(col)
+        else:
+            if len(df_train_name[col].unique()) < 4: #making anything with less than 4 unique values a catergorical value
+                col_cat.append(col)
+            else:
+                col_num.append(col)
+    return col_cat , col_num           
+#the call for this should be: prep.cat_and_num_lists(df_train_name, column_range)
+
+
+#This function is for running through catagorical on catagorical features graphing and running the chi2 test on them (by Alexia)
+def cat_on_cat_graph_loop(dataframe_train_name, col_cat, target_ver, target_ver_column_name):
     for col in col_cat:
         print()
         print(col.upper())
-        print(dataframe_name[col].value_counts())
-        print(dataframe_name[col].value_counts(normalize=True))
-        dataframe_name[col].value_counts().plot.bar()
+        print(dataframe_train_name[col].value_counts())
+        print(dataframe_train_name[col].value_counts(normalize=True))
+        dataframe_train_name[col].value_counts().plot.bar()
         plt.show()
         print()
         print()
         print(f'HYPOTHESIZE')
-        print(f"H_0: {col.lower().replace('_',' ')} does not survival")
-        print(f"H_a: {col.lower().replace('_',' ')} affects survival")
+        print(f"H_0: {col.lower().replace('_',' ')} does not affect {target_ver}")
+        print(f"H_a: {col.lower().replace('_',' ')} affects {target_ver}]")
         print()
         print(f'VISUALIZE')
-        sns.barplot(x=dataframe_train_name[col], y=dataframe_train_name['survived'])
-        plt.title(f"{col.lower().replace('_',' ')} vs Survive")
+        sns.barplot(x=dataframe_train_name[col], y=dataframe_train_name[target_ver_column_name])
+        plt.title(f"{col.lower().replace('_',' ')} vs {target_ver}")
         plt.show()
         print()
         print('ANALYZE and SUMMARIZE')
-        observed = pd.crosstab(dataframe_train_name[col], dataframe_train_name.survived)
+        observed = pd.crosstab(dataframe_train_name[col], dataframe_train_name[target_ver_column_name])
         chi2Test(observed)
         print()
         print()
-#the call should be: prep.cat_on_cat_graph_loop(dataframe_name, dataframe_train_name, col_cat)        
+#the call should be: prep.cat_on_cat_graph_loop(dataframe_train_name, col_cat, target_ver, target_ver_column_name)        
 
 #this funciton works in this module to run the chi2 test with the above function
 def chi2Test(observed):
